69335c15a6916f8d6c367e9ecbfc20642fec4f5c
Adeen Mir <adeenmir991@gmail.com>
2025-07-29 15:02:07 +0500

    calc

--- BJSC/script.js
+++ BJSC/script.js
@@ -266,13 +266,14 @@ that spans multiple lines*/
 // const person1 = new Person("Adeen", 22, "123 Main St", "New York", "USA");
 // console.log(person1.address.street);
 
-const cards=[`A`,2,3,4,5,6,7,8,9,10,`J`,`Q`,`K`];
-shuffle(cards);
+// const cards=[`A`,2,3,4,5,6,7,8,9,10,`J`,`Q`,`K`];
+// shuffle(cards);
 
-console.log(cards);
-function shuffle(array){
-    for(let i=array.length-1;i>0;i--){
-        const random=Math.floor(Math.random()*(i+1));
-    [array[i],array[random]]= [array[random],array[i]];
-    }
-}
+// console.log(cards);
+// function shuffle(array){
+//     for(let i=array.length-1;i>0;i--){
+//         const random=Math.floor(Math.random()*(i+1));
+//     [array[i],array[random]]= [array[random],array[i]];
+//     }
+// }
+
